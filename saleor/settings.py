a05b4e2ce25ffb8f1eb958c0b7b32c78865f7b94
IKarbowiak <iga.karbowiak@mirumee.com>
2022-06-06 15:32:48 +0200

    Add thumbnail app

--- saleor/settings.py
+++ saleor/settings.py
@@ -226,6 +226,7 @@ INSTALLED_APPS = [
     "saleor.warehouse",
     "saleor.webhook",
     "saleor.app",
+    "saleor.thumbnail",
     # External apps
     "versatileimagefield",
     "django_measurement",
